adb2ce570a9fc5f18a31093ef149d40924b14398
svaddi <chinnitpt@yahoo.com>
2013-08-08 17:18:33 -0500

    Create a class containing an int and a char that are not initialized, and print their values to verify that Java performs default initialization.

--- EveryThingIsAnObject/Exercise1.java
+++ EveryThingIsAnObject/Exercise1.java
@@ -1,3 +1,7 @@
+/**
+ * Create a class containing an int and a char that are not initialized, and print their values to verify that Java 
+ * performs default initialization.
+ */
 package EveryThingIsAnObject;
 
 /**
