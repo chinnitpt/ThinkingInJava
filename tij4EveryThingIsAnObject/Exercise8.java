8b7bbced74b4fee66e3cddfb1507cdebe57253c9
svaddi <chinnitpt@yahoo.com>
2013-08-08 22:02:48 -0500

    Write a program that demonstrates that, no matter how many objects you create of a particular class, there is only one instance of a particular static field in that class.

--- tij4EveryThingIsAnObject/Exercise8.java
+++ tij4EveryThingIsAnObject/Exercise8.java
@@ -13,4 +13,17 @@ package tij4EveryThingIsAnObject;
  * To change this template use File | Settings | File Templates.
  */
 class Exercise8 {
+    static int i = 10;
+
+    public static void main(String[] args) {
+        Exercise8 e8_1 = new Exercise8();
+        System.out.println("Static Variable accessed using object 1: " + e8_1.i);
+        e8_1.i++;
+        System.out.println("Value after incrementing the static variable accessed using object 1: " +  e8_1.i);
+
+        Exercise8 e8_2 = new Exercise8();
+        System.out.println("Static Variable accessed using object 2: " + e8_2.i);
+
+    }
+
 }
