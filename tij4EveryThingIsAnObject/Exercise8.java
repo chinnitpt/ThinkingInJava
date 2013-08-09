b32d5d9867bb63adffa147de362ebc386fa6c2a4
svaddi <chinnitpt@yahoo.com>
2013-08-08 21:47:23 -0500

    Write a program that demonstrates that, no matter how many objects you create of a particular class, there is only one instance of a particular static field in that class.

---
+++ tij4EveryThingIsAnObject/Exercise8.java
@@ -0,0 +1,11 @@
+package tij4EveryThingIsAnObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sukumar
+ * Date: 8/8/13
+ * Time: 9:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exercise8 {
+}
