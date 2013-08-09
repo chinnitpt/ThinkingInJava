30226474216cbd458cee96c6a2997716bb3f369f
svaddi <chinnitpt@yahoo.com>
2013-08-08 21:29:48 -0500

    Turn the Incrementable code fragments into a working program.

---
+++ EveryThingIsAnObject/Exercise7/StaticTest.java
@@ -0,0 +1,13 @@
+package EveryThingIsAnObject.Exercise7;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sukumar
+ * Date: 8/8/13
+ * Time: 6:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class StaticTest {
+        static int i = 47;
+}
+
