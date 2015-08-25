ad8a98090bd66a14a43e737d4130b15549ed4849
chinnitpt <svaddi01@HH0082014937.txhhsc.txnet.state.tx.us>
2015-08-25 11:55:28 -0500

    Exercise 13 Finished
    Javadoc generated for Documentation1.java, Documentation2.java and
    Documentation3.java and checked 'Doc' Folder

---
+++ tij4EveryThingIsAnObject/Exercise13/Documentation1.java
@@ -0,0 +1,5 @@
+package tij4EveryThingIsAnObject.Exercise13;
+
+public class Documentation1 {
+
+}
---
+++ tij4EveryThingIsAnObject/Exercise13/Documentation3.java
@@ -0,0 +1,13 @@
+package tij4EveryThingIsAnObject.Exercise13;
+
+public class Documentation3 {
+	//: object/Documentation3.java
+	/**
+	* You can <em>even</em> insert a list:
+	* <ol>
+	* <li> Item one
+	* <li> Item two
+	* <li> Item three
+	* </ol>
+	*/
+}
