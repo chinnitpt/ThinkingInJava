39eccc63f5e9ab5e923a87f37fbb9446a343922b
chinnitpt <svaddi01@HH0082014937.txhhsc.txnet.state.tx.us>
2015-08-25 12:32:09 -0500

    Finishing Exercise 15
    Generating documentation for exercise 15 which is a replica of exercise
    2

---
+++ tij4EveryThingIsAnObject/exercise15/Exercise15.java
@@ -0,0 +1,16 @@
+package tij4EveryThingIsAnObject.exercise15;
+
+/**
+ * 
+ * This class demonstrates 'HelloWorld' in java
+ * @author svaddi01
+ */
+public class Exercise15 {
+	/**
+	 * This is the method JVM loads and runs when you try to execute this program
+	 * @param args Array of String arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+	}
+}
