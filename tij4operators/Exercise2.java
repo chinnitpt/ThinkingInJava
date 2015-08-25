3d753465ea17a51aeaab08e10dd263deaa558922
chinnitpt <svaddi01@HH0082014937.txhhsc.txnet.state.tx.us>
2015-08-25 14:32:50 -0500

    Exercise 2 Finished

---
+++ tij4operators/Exercise2.java
@@ -0,0 +1,27 @@
+package tij4operators;
+import static net.mindview.util.Print.*;
+public class Exercise2 {
+	public static void main(String[] args)
+	{
+		float f1 = 23.4f;
+		float f2 = 24.4f;
+		print("f1:"+f1);
+		print("f2:"+f2);
+		
+		f1=f2;
+		print("f1:"+f1);
+		print("f2:"+f2);
+		
+		f1= 3.5f;
+		
+		print("f1:"+f1);
+		print("f2:"+f2);
+		
+		f2 = 2.5f;
+		print("f1:"+f1);
+		print("f2:"+f2);
+		
+		
+		
+	}
+}
